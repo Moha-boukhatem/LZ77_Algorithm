d157df635c816fbb5b2678b46caad9c9809282fb
moha-boukhatem <moha94orly@gmail.com>
2021-12-28 00:45:38 +0100

    Done!!!

--- TP1.py
+++ TP1.py
@@ -1,16 +1,11 @@
-S= "BRICABRACABRICEDENICE"
+S= "BRICABRACABRICEDENICE "
 new_chaine = ""
 chaine = len(S)
 gauche = ""
 droite = S
-i = 0
+i,k = 0,0
 
 
-
-'''
-position = 0
-droite = S
-'''
 # parcour la chaine droite
 
 while i < chaine :
@@ -18,48 +13,34 @@ while i < chaine :
     if len(gauche) >= 9 :
         gauche = gauche[len(gauche)-8:]
 
-    if S[i] in gauche  :
+    if S[i] in gauche  or  new_chaine !="":
             
         new_chaine += S[i]
         
-        position = len(gauche) - gauche.find(new_chaine)-2 
-        
-        if gauche.find(new_chaine) == -1 :
+                   
+        if gauche.find(new_chaine) == -1 or len(new_chaine) > 4:
             
-            gauche += new_chaine
+           
+            new_c = new_chaine[:-1]
+            position = len(gauche) - gauche.find(new_c)-1
+        
+
+            k+=1    
+            print(" {}. gauche = \"{}\" , droite = \"{}\" : code < {} , {} , \"{}\" >".format(k%10,gauche,droite,position,str(len(new_c)),S[i]))
+            
             if len(gauche) >= 9 :
                 gauche = gauche[len(gauche)-8:]
-            print("Gauche =\"",gauche,"\" , Droite =\"",droite,"\" : code <",position,",",str(len(new_chaine)),"",S[i],">")
+
+            gauche += new_chaine
             droite = S[i+1:]
             new_chaine =""
 
-
-
-        '''
-         
-            
-            len(new_chaine) = len(new_chaine)
-            position =len(gauche)-gauche.find(S[i])-1
-        
-         and new_chaine == gauche[position:position+len(new_chaine)]: 
-
-            new_chaine += S[i]
-            droite = S[i+1:]
-            gauche+=S[i]        
-        '''
-        
-    if S[i] not in gauche  :
-        print("Gauche =\"",gauche,"\" , Droite =\"",droite,"\" : code < 0 , 0 ",S[i],">")
+    if S[i] not in gauche  and new_chaine =="":
+        k+=1
+        print(" {}. gauche = \"{}\" , droite = \"{}\" : code < 0 , 0 , \"{}\" >".format(k,gauche,droite,S[i]))
         gauche+=S[i]
         droite = S[i+1:]
 
     i+=1
 
     
-
-    
-
-        
-
-
-
