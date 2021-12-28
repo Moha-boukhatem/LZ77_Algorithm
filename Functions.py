984e9b2e722f5023244dea75d5fef07845c41ee6
moha-boukhatem <moha94orly@gmail.com>
2021-12-28 01:56:25 +0100

    Add functions

--- Functions.py
+++ Functions.py
@@ -2,8 +2,8 @@
 def ShowRedundancySteps(k,gauche,droite,position=0,chaine="",lettre=""):
     
     length = str(len(chaine))
-    print(" {}. gauche = \" {} \" , droite = \" {}\" : code < {} , {} , \"{}\" >".format(k%10,gauche,droite,position,length,lettre))
+    print(" {}. gauche = \" {} \" , droite = \" {}\" : code < {},{},'{}'>".format(k%10,gauche,droite,position,length,lettre))
 
 def ShowNormalSteps(k,gauche,droite,lettre=""):
-    
-    print(" {}. gauche = \" {} \" , droite = \" {}\" : code < 0 , 0 , \"{}\" >".format(k%10,gauche,droite,lettre))
+
+    print(" {}. gauche = \" {} \" , droite = \" {}\" : code < 0,0,'{}'>".format(k%10,gauche,droite,lettre))
--- OurStatements.py
+++ OurStatements.py
@@ -1,2 +1,1 @@
-
 OriginalStatement= "BRICABRACABRICEDENICE "
