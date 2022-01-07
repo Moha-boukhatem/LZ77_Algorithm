7e926bb6d583fe5502e9aef8292b3c5f489de532
moha-boukhatem <moha94orly@gmail.com>
2022-01-07 19:56:13 +0100

    It's Work !!

--- src/LZ77_Algorithm.py
+++ src/LZ77_Algorithm.py
@@ -3,6 +3,8 @@ import Statements as Statements
 
 def Main():
     #LZ77_Compression_Function(Statements.OriginalStatement)
+
+    
     LZ77_Decompression_Function(Statements.OriginalStatement)
 
 
@@ -10,6 +12,9 @@ def Main():
 def LZ77_Compression_Function(Statement):
     
     gauche_list ,droite_list ,position_list ,size_list ,lettre_list= [],[],[],[],[]
+    print("\n ##################    Message Encoding   ##################\n")
+
+    PrintStatement(Statement)
 
 
     S= Statement
@@ -24,7 +29,7 @@ def LZ77_Compression_Function(Statement):
 
         if len(gauche) >= 9 :
             gauche = gauche[len(gauche)-8:]
-
+            
         if S[i] in gauche  or  new_chaine !="":
                 
             new_chaine += S[i]  
@@ -34,8 +39,7 @@ def LZ77_Compression_Function(Statement):
                 position = len(gauche) - gauche.find(new_c)-1
             
                 ShowRedundancySteps(k,gauche,droite,position,new_c,S[i])
-                gauche_list.append(gauche)
-                droite_list.append(droite)
+        
                 position_list.append(position)
                 size_list.append(len(new_c))
                 lettre_list.append(S[i])
@@ -65,15 +69,45 @@ def LZ77_Compression_Function(Statement):
 
         i+=1
 
-    return gauche_list ,droite_list ,position_list ,size_list ,lettre_list
-
+    return position_list ,size_list ,lettre_list
 def LZ77_Decompression_Function(Statement):
-    
-    data = LZ77_Compression_Function(Statement)
-  
 
-    gauche_list   = data[0]
-    droite_list   = data[1]
-    position_list = data[2]
-    size_list     = data[3]
-    lettre_list   = data[4]
+    data = LZ77_Compression_Function(Statement)
+    
+    print("\n ##################    Message Decoding    ##################\n")
+ 
+    position_list = data[0]
+    size_list     = data[1]
+    lettre_list   = data[2]
+
+    length = len(lettre_list)
+    chaine = ""
+    k,i = 0,0
+    
+    while i < length : 
+        
+        New_chaine =""
+
+        if position_list[i] == 0 and size_list[i] == 0 :
+            chaine += lettre_list[i]
+            
+            k+=1
+            ShowNormalDecryptSteps(k,chaine,lettre_list[i])    \
+        
+        else :
+            position = len(chaine)-position_list[i]-1
+            New_chaine =  chaine[position:position+size_list[i]]
+            
+            k+=1
+
+            chaine += New_chaine
+            chaine+= lettre_list[i]
+            ShowRedundancyDecryptSteps(k,chaine,position_list[i],size_list[i],lettre_list[i])
+
+
+        i+=1
+    
+    PrintStatement(Statement)
+
+    if chaine == Statement :
+        print("\n *** It's Done !!! ***\n")
--- src/PrintFunctions.py
+++ src/PrintFunctions.py
@@ -7,3 +7,17 @@ def ShowRedundancySteps(k,gauche,droite,position=0,chaine="",lettre=""):
 def ShowNormalSteps(k,gauche,droite,lettre=""):
 
     print(" {}. gauche = \" {} \" , droite = \" {}\" : code < 0,0,'{}'>".format(k,gauche,droite,lettre))
+
+
+def ShowRedundancyDecryptSteps(k,chaine,position,size,lettre=""):
+    
+    print(" {}. {}  : code < {},{},'{}'>".format(k,chaine,position,size,lettre))
+
+
+def ShowNormalDecryptSteps(k,chaine,lettre) :
+    print(" {}. {}  : code < 0,0,'{}'>".format(k,chaine,lettre))
+
+
+def PrintStatement(Statement):
+    print("\n Statement :  {} \n".format(Statement))
+
